7b12a9b91e3017c82f79ef818ff1885bdff9cbba
Diogo Miranda <diogo.miranda10@yahoo.com.br>
2018-03-12 10:39:34 -0300

    initial commit

---
+++ App/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  App
+//
+//  Created by Diogo Miranda on 3/3/18.
+//  Copyright © 2018 Diogo Miranda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func touch_button(_ sender: Any) {
+        
+        print("pressing button")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
